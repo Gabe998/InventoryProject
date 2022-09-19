1dc0762723cf363c222dffea1b99d53541b9f36a
Gabe998 <gabrieliusgintalas@csu.fullerton.edu>
2022-09-19 05:13:17 +0000

    second iteration

--- Inventory.cc
+++ Inventory.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include "products.h"
+#include <string>
 
 using namespace std;
 
@@ -15,44 +15,73 @@ string inventory = "/inventory";
 string add = "/add";
 string trash = "/remove";
 
+vector<int> prod_id;
+vector<int> prod_qty;
+vector<string> prod_name;
+
+bool toinventory = false;
+bool toadd = false;
+bool toremove = false;
+
+bool commands = false;
+
+void commandcall() {
+  if (command == inventory) {
+      toinventory = true;
+    } else if (command == add) {
+      toadd = true;
+    } else if (command == trash) {
+      toremove = false;
+    } else {
+      cout << "Invalid command \n";
+    }
+}
+
+
 void addproduct() {
       cout << "Please enter the Product ID you would like to use: ";
       cin >> productID;
+      prod_id.push_back(productID);
       cout << "Please enter the name of your product: ";
       cin >> productname;
+      prod_name.push_back(productname);
       cout << "Please enter the amount of your product: ";
       cin >> quantity;
+      prod_qty.push_back(quantity);
       cout << "Added " << quantity << " " << productname << "'s to your inventory" << endl << message << "\n";
+      commands = true;
       cin >> command;
     }
-  
+
+int x;
+
+string strproductID = to_string(productID); 
+
+size_t product_count = prod_id.size();
+
 void viewinventory() {
   cout << "Here is your inventory: \n";
-
+  cout << prod_id.at(1) << "\n";
+  /*
+  for (x = 0; x <= product_count; x++) {
+    cout << "Product ID: " << productID << " Product Name: " << productname << " Produce Quantity: " << quantity << "\n";
+  }
+  cout << message << "\n";
+  cin >> command;
+  */
 }
 
 int main(){
-  map<int, map<string, int>> productIDs;
-
-  map<int, map<string, int>>::iterator IDs;
-  map<string,int>::iterator product;
-
-  bool toinventory = false;
-  bool toadd = false;
-  bool toremove = false;
-
-
   cout << "Hello! Welcome to your Inventory!! Please add a few items to get started! \n";
   addproduct();
 
-  if (command == inventory) {
-    toinventory = true;
-  } else if (command == add) {
-    toadd = true;
-  } else if (command == trash) {
-    toremove = false;
-  } else {
-    cout << "Invalid command \n";
+  prod_id.push_back(productID);
+  prod_qty.push_back(quantity);
+  prod_name.push_back(productname);
+
+  if (commands == true) {
+    commandcall();
+    commands = false;
   }
 
   if (toinventory == true) {
@@ -66,15 +95,6 @@ int main(){
     toremove = false;
   } else {
     cout << "No commands found";
-  }
-
-  productIDs.insert(make_pair(productID, map<string, int>()));
-  productIDs[productID].insert(make_pair(productname, quantity));
-
-  for (IDs = productIDs.begin(); IDs != productIDs.end(); IDs++) {
-    for (product = IDs ->second.begin(); product != IDs->second.end(); product++){
-        cout << "The Product key is " << IDs->first << " The Product name is " << product->first << " with a total quantity of: " << product->second << "\n";
-    }
   }
   return 0;
 }
